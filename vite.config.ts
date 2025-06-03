0957aafed4e32f5ac76b620161d77a2e722ce157
Jady <52546324@qq.com>
2025-06-03 16:53:53 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),],
-  base: '/github-pages-test/',
+  // base: '/github-pages-test/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
