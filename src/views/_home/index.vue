9c48046e010390c0205e825d4059b913599cba58
Jady <52546324@qq.com>
2025-06-05 09:37:00 +0800

    element-ui 样式

--- src/views/_home/index.vue
+++ src/views/_home/index.vue
@@ -1,12 +1,29 @@
 <template>
     <el-button type="primary" @click="handleJump">cliack</el-button>
+    <el-button type="info" @click="handleMessage">Info</el-button>
 </template>
 <script lang='ts' setup>
- import {useRouter}from "vue-router"
+    import {useRouter}from "vue-router"
+    import { ElMessage, ElMessageBox } from 'element-plus'
+    import type { Action } from 'element-plus'
  const router = useRouter()
 
 const handleJump = () => {
     router.push('/')
+}
+
+const handleMessage = () => {
+    ElMessageBox.alert('This is a message', 'Title', {
+    // if you want to disable its autofocus
+    // autofocus: false,
+    confirmButtonText: 'OK',
+    callback: (action: Action) => {
+      ElMessage({
+        type: 'info',
+        message: `action: ${action}`,
+      })
+    },
+  })
 }
 </script>
 <style lang='scss' scoped>
