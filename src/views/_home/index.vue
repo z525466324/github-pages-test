edbd89e6eea91e46dc1287654961a966481bc25c
Jady <52546324@qq.com>
2025-06-05 09:29:58 +0800

    kkk

--- src/views/_home/index.vue
+++ src/views/_home/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-button type="primary" @click="handleJump">click</el-button>
+    <el-button type="primary" @click="handleJump">cliack</el-button>
 </template>
 <script lang='ts' setup>
  import {useRouter}from "vue-router"
