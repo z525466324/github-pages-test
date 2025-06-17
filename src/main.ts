46a18df8272810b53053c66abc1a404aa24561bf
Jady <52546324@qq.com>
2025-06-17 10:05:03 +0800

    fix: table添加列字段重复拦截

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import 'element-plus/dist/index.css'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "element-plus/dist/index.css";
 
-import router from '@/routes'
+console.log(import.meta);
+import router from "@/routes";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
